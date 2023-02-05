63f5523875ac19e8090ed27779fa437f2d8972cb
SoundStrider <sords.will@gmail.com>
2023-02-05 15:43:02 -0600

    health pickup SFX

--- CATASTROPHE/Assets/Scripts/HealthPickup.cs
+++ CATASTROPHE/Assets/Scripts/HealthPickup.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class HealthPickup : MonoBehaviour
 {
     [SerializeField] private int healAmount = 1;
+    [SerializeField] private AudioClip healthPickupSFX;
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            AudioManager.instance.PlayerSFXPlayer(healthPickupSFX);
+
+
             other.transform.gameObject.GetComponent<PlayerHealth>().GainHealth(healAmount);
             Destroy(this.gameObject);
         }
